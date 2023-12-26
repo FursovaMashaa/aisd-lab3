3497cc443d127a66c61d437f7db153cb8d037191
FursovaMashaa <fursovamaria1@gmail.com>
2023-12-26 17:25:17 +0400

    merge_sort

--- sorting.cpp
+++ sorting.cpp
@@ -12,17 +12,35 @@ namespace sorts {
         size_t copy_count = 0;
     };
 
-    void quick_sort(vector<int>& list, int left, int right, stats& s) {
+    void insertionSort(vector<int>& list, stats& s) {
+        for (int i = 1; i < list.size(); i++) {
+            int key = list[i];
+            int j = i - 1;
+
+            while (j >= 0 && list[j] > key) {
+                list[j + 1] = list[j];
+                j--;
+                s.comparison_count++;
+                s.copy_count++;
+            }
+
+            list[j + 1] = key;
+            s.copy_count++;
+        }
+    }
+
+
+    void quickSort(vector<int>& list, int left, int right, stats& s) {
         if (left > right) { return; }
         int pivot_point = list[(left + right) / 2];
         int i = left;
         int j = right;
         while (i <= j) {
-            while (list[i] < pivot_point) { 
-                i++; s.comparison_count++; 
+            while (list[i] < pivot_point) {
+                i++; s.comparison_count++;
             }
-            while (list[j] > pivot_point) { 
-                j--; s.comparison_count++; 
+            while (list[j] > pivot_point) {
+                j--; s.comparison_count++;
             }
             if (i <= j) {
                 int t = list[i];
@@ -34,7 +52,47 @@ namespace sorts {
             }
 
         }
-        quick_sort(list, left, j, s);
-        quick_sort(list, i, right, s);
+        quickSort(list, left, j, s);
+        quickSort(list, i, right, s);
     }
+
+    void merge_sort(vector<int>& list, stats& s) {
+        if (list.size() <= 1) {
+            return;
+        }
+
+        vector<int> left(list.begin(), list.begin() + list.size() / 2);
+        vector<int> right(list.begin() + list.size() / 2, list.end());
+
+        merge_sort(left, s);
+        merge_sort(right, s);
+
+        vector<int> merged(list.size());
+        int i = 0, j = 0;
+        while (i < left.size() && j < right.size()) {
+            s.comparison_count++;
+            if (left[i] < right[j]) {
+                merged[i + j] = left[i];
+                i++;
+            }
+            else {
+                merged[i + j] = right[j];
+                j++;
+            }
+        }
+        while (i < left.size()) {
+            merged[i + j] = left[i];
+            i++;
+        }
+        while (j < right.size()) {
+            merged[i + j] = right[j];
+            j++;
+        }
+
+        for (int k = 0; k < list.size(); k++) {
+            list[k] = merged[k];
+            s.copy_count++;
+        }
+    }
+
 };
--- tests.cc
+++ tests.cc
@@ -6,10 +6,35 @@
 using namespace std;
 using namespace sorts;
 
-TEST(Test1, quick_sort) {
+
+TEST(Test1, insertionSort) {
 	vector<int>a = { 4,6,8,13,2,1,1,5,9 };
 	vector<int>b = { 1,1,2,4,5,6,8,9,13 };
 	stats s;
-	quick_sort(a, 0, a.size() - 1, s);
+	insertionSort(a, s);
 	EXPECT_EQ(a, b);
 }
+
+TEST(Test2, quickSort) {
+	vector<int>a = { 4,6,8,13,2,1,1,5,9 };
+	vector<int>b = { 1,1,2,4,5,6,8,9,13 };
+	stats s;
+	quickSort(a, 0, a.size() - 1, s);
+	EXPECT_EQ(a, b);
+};
+
+TEST(Test3, merge_sort) {
+	vector<int>a = { 4,6,8,13,2,1,1,5,9 };
+	vector<int>b = { 1,1,2,4,5,6,8,9,13 };
+	stats s;
+	merge_sort(a,s);
+	EXPECT_EQ(a, b);
+};
+
+TEST(Test4, merge_sort2) {
+	vector<int>a = { 4,6,8,13,2,1,1,5,9,34,0,24,11,66,45 };
+	vector<int>b = { 0,1,1,2,4,5,6,8,9,11,13,24,34,45,66 };
+	stats s;
+	merge_sort(a, s);
+	EXPECT_EQ(a, b);
+};
